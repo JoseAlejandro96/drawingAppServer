a6dbe790ecffd43c6e740bbe1e68d4e7699e5943
JoseAlejandro96 <34602875+JoseAlejandro96@users.noreply.github.com>
2021-06-29 15:35:08 +0200

    implementing phases and Broadcasting DrawData

--- src/routes/WebSocketRoutes.kt
+++ src/routes/WebSocketRoutes.kt
@@ -1,10 +1,14 @@
 package com.joshportfolio.routes
 
 import com.google.gson.JsonParser
+import com.joshportfolio.data.Room
 import com.joshportfolio.data.models.BaseModel
 import com.joshportfolio.data.models.ChatMessage
+import com.joshportfolio.data.models.DrawData
 import com.joshportfolio.gson
 import com.joshportfolio.other.Constants.TYPE_CHAT_MESSAGE
+import com.joshportfolio.other.Constants.TYPE_DRAW_DATA
+import com.joshportfolio.server
 import com.joshportfolio.session.DrawingSession
 import io.ktor.http.cio.websocket.*
 import io.ktor.routing.*
@@ -18,6 +22,24 @@ import java.lang.Exception
     standar websocket - intercepta todo el trafico y lo redirige a otra funcion que decide
     *                               que hacer con el trafico
 * */
+
+fun Route.gameWebSocketRoute(){
+    route("/ws/draw"){
+        standardWebSocket { socket, clientId, message, payload ->
+            when(payload){
+                is DrawData ->{
+                    val room = server.rooms[payload.roomName] ?: return@standardWebSocket
+                    if(room.phase == Room.Phase.GAME_RUNNING){
+                        room.broadcastToAllExcept(message, clientId)
+                    }
+                }
+                is ChatMessage ->{
+
+                }
+            }
+        }
+    }
+}
 
 fun Route.standardWebSocket(
     handleFrame: suspend (
@@ -40,6 +62,7 @@ fun Route.standardWebSocket(
                     val jsonObject = JsonParser.parseString(message).asJsonObject
                     val type = when(jsonObject.get("type").asString){
                         TYPE_CHAT_MESSAGE -> ChatMessage::class.java
+                        TYPE_DRAW_DATA -> DrawData::class.java
                         else -> BaseModel::class.java
                     }
                     val payload = gson.fromJson(message, type)
